811236ecbbfa5a720aa7da077fc6b10542546233
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-26 21:03:32 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState(null)
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id))
+  }
+
+  useEffect(() => {
+  //  https://6308fcaff8a20183f76cccfc.mockapi.io/users
+    axios.get('https://6308fcaff8a20183f76cccfc.mockapi.io/users')
+      .then(function (response) {
+        // handle success
+        setUsers(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        users ? users.map((user) => {
+          return(<div key={user.id}>
+            <h5>{user.name}</h5>
+            <button onClick={() => deleteUser(user.id)}>Remove</button>
+          </div>)
+        }) : 0
+      }
     </div>
   );
 }
